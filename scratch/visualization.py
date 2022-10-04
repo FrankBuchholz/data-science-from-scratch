8ae784230bbe8ff796e135aa451dee5347e3953c
FrankBuchholz <frank.buchholz@sap.com>
2022-10-04 16:56:18 +0200

    Update chapter 3

--- scratch/visualization.py
+++ scratch/visualization.py
@@ -1,9 +1,12 @@
 from matplotlib import pyplot as plt
 
-# ---
+# --- matplotlib
 
 years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
 gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+
+# define / select figure
+plt.figure("3-1")
 
 # create a line chart, years on x-axis, gdp on y-axis
 plt.plot(years, gdp, color='green', marker='o', linestyle='solid')
@@ -13,15 +16,17 @@ plt.title("Nominal GDP")
 
 # add a label to the y-axis
 plt.ylabel("Billions of $")
-#plt.savefig('im/viz_gdp.png')
+plt.savefig('im/03-01_viz_gdp.png')
 plt.show()
+plt.close()
 
-# ---
-
-plt.gca().clear()
+# --- Balkendiagramme
 
 movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
 num_oscars = [5, 11, 3, 8, 10]
+
+# define / select figure
+plt.figure("3-2")
 
 # plot bars with left x-coordinates [0, 1, 2, 3, 4], heights [num_oscars]
 plt.bar(range(len(movies)), num_oscars)
@@ -32,18 +37,18 @@ plt.ylabel("# of Academy Awards")   # label the y-axis
 # label x-axis with movie names at bar centers
 plt.xticks(range(len(movies)), movies)
 
-#plt.savefig('im/viz_movies.png')
+plt.savefig('im/03-02_viz_movies.png')
 plt.show()
+plt.close()
 
-# ---
-
-plt.gca().clear()
 
 from collections import Counter
 grades = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0]
 
 # Bucket grades by decile, but put 100 in with the 90s
 histogram = Counter(min(grade // 10 * 10, 90) for grade in grades)
+
+plt.figure("3-3")
 
 plt.bar([x + 5 for x in histogram.keys()],  # Shift bars right by 5
         histogram.values(),                 # Give each bar its correct height
@@ -57,16 +62,15 @@ plt.xticks([10 * i for i in range(11)])    # x-axis labels at 0, 10, ..., 100
 plt.xlabel("Decile")
 plt.ylabel("# of Students")
 plt.title("Distribution of Exam 1 Grades")
-#plt.savefig('im/viz_grades.png')
+plt.savefig('im/03-03_viz_grades.png')
 plt.show()
+plt.close()
 
-# ---
-
-plt.gca().clear()
 
 mentions = [500, 505]
 years = [2017, 2018]
 
+plt.figure("3-4")
 plt.bar(years, mentions, 0.8)
 plt.xticks(years)
 plt.ylabel("# of times I heard someone say 'data science'")
@@ -78,13 +82,12 @@ plt.ticklabel_format(useOffset=False)
 # misleading y-axis only shows the part above 500
 plt.axis([2016.5, 2018.5, 499, 506])
 plt.title("Look at the 'Huge' Increase!")
-#plt.savefig('im/viz_misleading_y_axis.png')
+plt.savefig('im/03-04_viz_misleading_y_axis.png')
 plt.show()
+plt.close()
 
-# ---
 
-plt.gca().clear()
-
+plt.figure("3-5")
 plt.bar(years, mentions, 0.8)
 plt.xticks(years)
 plt.ylabel("# of times I heard someone say 'data science'")
@@ -92,18 +95,18 @@ plt.ticklabel_format(useOffset=False)
 
 plt.axis([2016.5, 2018.5, 0, 550])
 plt.title("Not So Huge Anymore")
-#plt.savefig('im/viz_non_misleading_y_axis.png')
+plt.savefig('im/03-05_viz_non_misleading_y_axis.png')
 plt.show()
+plt.close()
 
-# ---
-
-plt.gca().clear()
+# --- Liniendiagramme
 
 variance     = [1, 2, 4, 8, 16, 32, 64, 128, 256]
 bias_squared = [256, 128, 64, 32, 16, 8, 4, 2, 1]
 total_error  = [x + y for x, y in zip(variance, bias_squared)]
 xs = [i for i, _ in enumerate(variance)]
 
+plt.figure("3-6")
 # We can make multiple calls to plt.plot
 # to show multiple series on the same chart
 plt.plot(xs, variance,     'g-',  label='variance')    # green solid line
@@ -116,17 +119,17 @@ plt.legend(loc=9)
 plt.xlabel("model complexity")
 plt.xticks([])
 plt.title("The Bias-Variance Tradeoff")
-#plt.savefig('im/viz_line_chart.png')
+plt.savefig('im/03-06_viz_line_chart.png')
 plt.show()
+plt.close()
 
-# ---
-
-plt.gca().clear()
+# --- Scatterplots
 
 friends = [ 70,  65,  72,  63,  71,  64,  60,  64,  67]
 minutes = [175, 170, 205, 120, 220, 130, 105, 145, 190]
 labels =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 
+plt.figure("3-7")
 plt.scatter(friends, minutes)
 
 # label each point
@@ -139,37 +142,33 @@ for label, friend_count, minute_count in zip(labels, friends, minutes):
 plt.title("Daily Minutes vs. Number of Friends")
 plt.xlabel("# of friends")
 plt.ylabel("daily minutes spent on the site")
-#plt.savefig('im/viz_scatterplot.png')
+plt.savefig('im/03-07_viz_scatterplot.png')
 plt.show()
+plt.close()
 
-# ---
-
-plt.gca().clear()
 
 test_1_grades = [ 99, 90, 85, 97, 80]
 test_2_grades = [100, 85, 60, 90, 70]
 
+plt.figure("3-8")
 plt.scatter(test_1_grades, test_2_grades)
 plt.title("Axes Aren't Comparable")
 plt.xlabel("test 1 grade")
 plt.ylabel("test 2 grade")
-#plt.savefig('im/viz_scatterplot_axes_not_comparable.png')
+plt.savefig('im/03-08_viz_scatterplot_axes_not_comparable.png')
 plt.show()
+plt.close()
 
-# ---
-
-plt.gca().clear()
 
 test_1_grades = [ 99, 90, 85, 97, 80]
 test_2_grades = [100, 85, 60, 90, 70]
+
+plt.figure("3-9")
 plt.scatter(test_1_grades, test_2_grades)
 plt.title("Axes Are Comparable")
 plt.axis("equal")
 plt.xlabel("test 1 grade")
 plt.ylabel("test 2 grade")
-#plt.savefig('im/viz_scatterplot_axes_comparable.png')
+plt.savefig('im/03-09_viz_scatterplot_axes_comparable.png')
 plt.show()
-
-# ---
-
-plt.gca().clear()
+plt.close()
